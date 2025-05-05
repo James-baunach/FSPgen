139955d7d10074b50dbef0c23438ddd902ae7dd9
James-baunach <james.baunach@gmail.com>
2025-05-04 19:32:03 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'fsp-form-cache-v1'; // Increment version if you change cached files
+const CACHE_NAME = 'fsp-form-cache-v2'; // Increment version if you change cached files
 const urlsToCache = [
   '.', // Alias for index.html
   'index.html',
